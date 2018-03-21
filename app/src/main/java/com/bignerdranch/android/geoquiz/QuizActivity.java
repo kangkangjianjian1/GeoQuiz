88fa0a0f28369ec8731a2876c1788b935a027420
kangkangjianjian1 <1020307642@qq.com>
2018-03-21 20:58:33 +0800

    First commit on  add Function Toast.setGravity  dev branch!

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,7 +20,11 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //doNothing but soon
-                Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+               Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT);
+                toast .setGravity(Gravity.CENTER,0,0);
+                toast.show();
+
+
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +32,9 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //DoNothing but soon
 
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+             Toast toast =   Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,10,10);
+                toast.show();
             }
         });
 
